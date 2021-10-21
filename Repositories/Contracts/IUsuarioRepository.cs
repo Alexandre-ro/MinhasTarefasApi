e34172b3f72d12965ec24259bd4661a1e990cf98
Alexandre-ro <alexandre.programador84@gmail.com>
2021-10-20 22:46:56 -0300

    Criados a interface IUserRepository e a classe UserRepository.

---
+++ Repositories/Contracts/IUsuarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using MinhasTarefasApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhasTarefasApi.Repositories.Contracts
+{
+    public interface IUsuarioRepository
+    {
+        void Cadastrar(ApplicationUser Usuario, string senha);
+        ApplicationUser Obter(string email, string senha);
+    }
+}
---
+++ Repositories/UsuarioRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MinhasTarefasApi.Models;
+using MinhasTarefasApi.Repositories.Contracts;
+using System;
+using System.Text;
+
+namespace MinhasTarefasApi.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UsuarioRepository(UserManager<ApplicationUser> userManager)
+        {
+
+            _userManager = userManager;
+        }
+
+        public ApplicationUser Obter(string email, string senha)
+        {
+            var usuario = _userManager.FindByIdAsync(email).Result;
+
+            if (_userManager.CheckPasswordAsync(usuario, senha).Result)
+            {
+                return usuario;
+            }
+
+            else
+            {
+                throw new Exception("Usuário não encontrado!");
+            }
+        }
+
+        public void Cadastrar(ApplicationUser usuario, string senha)
+        {
+            var result = _userManager.CreateAsync(usuario, senha).Result;
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var erro in result.Errors) 
+            {
+                sb.Append(erro.Description);
+            }
+
+            if (!result.Succeeded)
+            {
+                throw new Exception($"Usuário não cadastrado! {sb.ToString()}");
+            }
+        }
+    }
+}
